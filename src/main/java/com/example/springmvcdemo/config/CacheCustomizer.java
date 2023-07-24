8d9479bada786df343a4019f9a7b55c0ecca036e
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2023-07-24 16:07:32 +0600

    Add spring boot cache config

---
+++ src/main/java/com/example/springmvcdemo/config/CacheCustomizer.java
@@ -0,0 +1,20 @@
+package com.example.springmvcdemo.config;
+
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CacheCustomizer
+        implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
+
+    @Override
+    public void customize(ConcurrentMapCacheManager cacheManager) {
+        cacheManager.setCacheNames(Arrays.asList("categories"));
+
+    }
+}
