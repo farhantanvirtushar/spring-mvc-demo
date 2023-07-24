440dcfe2ac6d96604eb6b2ac220ce8b19b5c7be9
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2023-07-24 12:24:59 +0600

    Fix StoryCategoryRepository bean creation error

--- src/main/java/com/example/springmvcdemo/repositories/StoryCategoryRepository.java
+++ src/main/java/com/example/springmvcdemo/repositories/StoryCategoryRepository.java
@@ -1,8 +1,9 @@
 package com.example.springmvcdemo.repositories;
 
+import com.example.springmvcdemo.model.StoryCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StoryCategoryRepository extends JpaRepository<StoryCategoryRepository,Long> {
+public interface StoryCategoryRepository extends JpaRepository<StoryCategory,Long> {
 }
